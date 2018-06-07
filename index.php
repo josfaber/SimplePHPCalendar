8a15a9e7ac76ce56bcc1539bf783eb6748746a56
Jos Faber <jfaber@protonmail.com>
2018-06-07 17:28:37 +0200

    no message

--- index.php
+++ index.php
@@ -1,35 +1,75 @@
 <?php
+// for dev purpose
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-/**
- * Require the class
- */
-require './simplephpcalendar.php';
+// set locale for date labels
+setlocale(LC_TIME, 'NL_nl');
 
-/**
- * Instantiate a calendar object
- */
-$the_date = isset($_GET["d"]) ? $_GET["d"] : date('Y-m-d');
-$cal = new JF\SimplePHPCalendar( $the_date );
-$cal->link = 'index.php';
+// Require the class
+require './simplephpcalendar.php';
 ?>
 
 <html>
   <head>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
     <style>
-      body { font-family: 'Open Sans', sans-serif; }
-      table.calendar a { color: #444; }
-      table.calendar td.today = { font-weight: 700; }
+      body { font-family: 'Open Sans', sans-serif; background: #efeff2; padding: 40px; }
+      hr { margin: 40px;}
+      .spc__table, .spc__table th, .spc__table td { box-sizing: border-box; }
+      .spc__table { padding: 8px; background: #fff; box-shadow: 0px 0px 32px 0px rgba(0,0,0,0.2); }
+      .spc__table a { color: #444; text-decoration: none;}
+      .spc__table th.spc__table__day { color:#999; font-size: 0.8em; font-weight: normal;}
+      .spc__table td.spc__table__day { width:32px; height:32px; background: #dde; text-align: center; font-size: 0.8em; padding: 4px;}
+      .spc__table td.spc__table__today { border: 2px solid #00a; color:#000; }
+      .spc__table td.spc__table__selected { background:#00a; font-weight: 700; }
+      .spc__table td.spc__table__selected a { color:#fff; }
+
+      /* second calendar collection */
+      .spc__table.year-cal { box-shadow: none; }
+      .spc__table.year-cal td.spc__table__today { background: #777; color:#fff; border: none;}
+      .spc__table.year-cal td.spc__table__day { width:24px; height:24px; background: #dde; text-align: center; font-size: 0.7em; padding: 4px;}
+      .spc__table.year-cal td.spc__table__today { background: #777; color:#fff; }
+      .spc__table.year-cal td.spc__table__selected { background:#000; font-weight: 700; }
+      .spc__table.year-cal td.spc__table__selected a { color:#fff; }
     </style>
   </head>
   <body>
+
     <?php
-    /**
-     * Draw the calendar
-     */
+    // instantiate calendar with options
+    $cal = new JF\SimplePHPCalendar([
+      "link"              => 'index.php',
+      "monthname_format"  => '%B', // default
+      "daylabel_format"   => '%a', // default
+      "date_param"        => 'date',
+    ]);
+
+    // Draw the calendar
     $cal->draw();
     ?>
+
+    <hr>
+
+    <?php
+    // example draw multiple calendars with containing class
+    for ($m=1; $m<=12; $m++) {
+      // instantiate
+      $cal = new JF\SimplePHPCalendar([
+          "link"              => 'index.php',
+          "monthname_format"  => '%B', // default
+          "daylabel_format"   => '%a', // default
+          "date_param"        => 'date',
+          "class_names"       => 'year-cal',
+          "style_today"       => FALSE,
+        ],
+        "2018-".str_pad($m, 2, "0", STR_PAD_LEFT)
+      );
+
+      // draw it
+      $cal->draw();
+    }
+    ?>
+
   </body>
 </html>
--- simplephpcalendar.php
+++ simplephpcalendar.php
@@ -1,36 +1,60 @@
 <?php
 namespace JF;
 
+/**
+ * @TODO
+ *
+ * monthname date format
+ *
+ */
 class SimplePHPCalendar {
 
-  public $link;
   public $year;
   public $month;
+
+  private $options;
+  private $defaults = [
+    "link"              => '',
+    "monthname_format"  => '%B',
+    "daylabel_format"   => '%a',
+    "date_param"        => 'd',
+    "class_names"       => "",
+    "style_today"       => TRUE,
+  ];
 
   private $running_day;
   private $days_in_month;
   private $month_name;
   private $month_pad;
+  private $selected_date;
   private $calDB = [];
 
-  public function __construct($the_date = null) {
-    // var_dump($the_date);
-    // var_dump(strtotime($the_date), date('Y-m-d', strtotime($the_date)));
+  public function __construct($options = [], $the_date = null, $selected_date = null) {
+    $this->options = array_replace_recursive($this->defaults, $options);
+    if (is_null($the_date)) {
+      $the_date = isset($_GET[$this->options["date_param"]]) ? $_GET[$this->options["date_param"]] : date('Y-m-d');
+    }
     $time = strtotime($the_date);
     $this->year = date('Y', $time);
     $this->month = date('m', $time);
     $this->day = date('d', $time);
     $this->running_day = date('w', mktime(0,0,0,$this->month,1,$this->year));
   	$this->days_in_month = date('t', mktime(0,0,0,$this->month,1,$this->year));
-  	$this->month_name = strftime('%b', mktime(0,0,0,$this->month,1,$this->year));
   	$this->month_pad = str_pad($this->month, 2, "0", STR_PAD_LEFT);
     $this->today = date("Y-m-d");
+    $this->selected_date = !is_null($selected_date) && $this->valiDate($selected_date) ? $selected_date : ($this->valiDate($the_date) ? date("Y-m-d", $time) : null);
+    $this->labels = [];
+  }
+
+  public function valiDate($date) {
+    return preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$date);
   }
 
   public function draw() {
-  	$this->weekdays = 1;
-  	$this->day_counter = 0;
-  	$this->labels = array("Zo", "Ma", "Di", "Wo", "Do", "Vr", "Za");
+    $this->weekdays = 1;
+    $this->day_counter = 0;
+    $this->month_name = strftime($this->options["monthname_format"], mktime(0,0,0,$this->month,1,$this->year));
+    for ($n=1; $n<8; $n++) { array_push($this->labels, strftime($this->options["daylabel_format"], mktime(0,0,0,4,$n,2018))); }
 
   	for($d=1;$d<=$this->days_in_month;$d++)
   	{
@@ -39,38 +63,38 @@ class SimplePHPCalendar {
   		$this->day_end =  mktime(23,59,59,$this->month,$d,$this->year);
   	}
   	?>
-  	<table class="calendar">
+  	<table class="spc__table<?php echo " {$this->options["class_names"]} "; ?>">
   		<thead>
   			<tr>
-  				<th class="title" colspan="7">
+  				<th class="spc__table__title" colspan="7">
 						<?php echo $this->month_name; ?>
   					<span class="year"><?php echo $this->year; ?></span>
   				</th>
   			</tr>
   			<tr>
-  				<th class="day"><?php echo implode("</th><th class=\"day\">", $this->labels); ?></th>
+  				<th class="spc__table__day"><?php echo implode("</th><th class=\"spc__table__day\">", $this->labels); ?></th>
   			</tr>
   		</thead>
   		<tbody>
   		<tr>
   			<?php
   			for($x = 0; $x < $this->running_day; $x++):
-  				echo "<td class=\"empty\">&nbsp;</td>";
+  				echo "<td class=\"spc__table__empty\">&nbsp;</td>";
   			 	$this->weekdays++;
   			endfor;
   			for($list_day = 1; $list_day <= $this->days_in_month; $list_day++):
   				$list_day_pad = str_pad($list_day, 2, "0", STR_PAD_LEFT);
-  				$isToday = $this->today === date("Y-m-d", mktime(0,0,0,$this->month,$list_day,$this->year));
-  				$isSelected = $this->day === date("Y-m-d", mktime(0,0,0,$this->month,$list_day,$this->year));
-          $classes = [];
-          !$isToday || array_push($classes, "today");
-          !$isSelected || array_push($classes, "selected");
-  				?>
+  				$isToday = $this->options["style_today"] ? $this->today === date("Y-m-d", mktime(0,0,0,$this->month,$list_day,$this->year)) : FALSE;
+  				$isSelected = $this->selected_date === date("Y-m-d", mktime(0,0,0,$this->month,$list_day,$this->year));
+          $classes = ["spc__table__day"];
+          !$isToday || array_push($classes, "spc__table__today");
+          !$isSelected || array_push($classes, "spc__table__selected");
+          ?>
   				<td class="<?php echo implode(" ", $classes); ?>">
-  					<div class="day">
-  						<?php if($this->link): ?><a href="<?php echo "{$this->link}?d={$this->year}-{$this->month_pad}-{$list_day_pad}"?>"><?php endif; ?>
+  					<div class="spc__table__day">
+  						<?php if($this->options["link"]): ?><a href="<?php echo "{$this->options["link"]}?{$this->options["date_param"]}={$this->year}-{$this->month_pad}-{$list_day_pad}"?>"><?php endif; ?>
                 <?php echo $list_day?>
-              <?php if($this->link): ?></a><?php endif; ?>
+              <?php if($this->options["link"]): ?></a><?php endif; ?>
   					</div>
   				</td>
   				<?php
@@ -88,7 +112,7 @@ class SimplePHPCalendar {
   			endfor;
   			if($this->weekdays > 1 && $this->weekdays < 8):
   				for($x = 1; $x <= (8 - $this->weekdays); $x++):
-  					echo "<td class=\"empty\"> </td>";
+  					echo "<td class=\"spc__table__empty\"> </td>";
   				endfor;
   			endif;
   			?>
